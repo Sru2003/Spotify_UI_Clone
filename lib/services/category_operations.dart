ea95f13e809b82c64037bb1fbc3f51627cd789a2
Srushti <srushtipednekar15@gmail.com>
2023-05-12 13:30:18 +0530

    Final Commit

--- lib/services/category_operations.dart
+++ lib/services/category_operations.dart
@@ -9,7 +9,7 @@ class CategoryOperations {
       Category('Hip Hop Mix','https://cachedimages.podchaser.com/256x256/aHR0cHM6Ly9kM3dvNXdvanZ1djdsLmNsb3VkZnJvbnQubmV0L3RfcnNzX2l0dW5lc19zcXVhcmVfMTQwMC9pbWFnZXMuc3ByZWFrZXIuY29tL29yaWdpbmFsL2U4ZDIzNmFlYzI5ZGE3MmY2NTQ0Mzg2OTRkNDc4MjMxLmpwZw%3D%3D/aHR0cHM6Ly93d3cucG9kY2hhc2VyLmNvbS9pbWFnZXMvbWlzc2luZy1pbWFnZS5wbmc%3D'),
       Category('Chill Hits','https://icon-library.com/images/cool-music-icon/cool-music-icon-9.jpg'),
       Category('Bollywood Hits','https://a10.gaanacdn.com/gn_pl_img/playlists/jBr3gybR1m/r3gex0lJbR/size_m_1515080889.webp'),
-      Category('Pritam','https://a10.gaanacdn.com/gn_img/artists/zLp36PvbrG/Lp36AR0KrG/size_xl_1516707336.webp'),
+      Category('Pritam ','https://a10.gaanacdn.com/gn_img/artists/zLp36PvbrG/Lp36AR0KrG/size_xl_1516707336.webp'),
 
     ];
   }
